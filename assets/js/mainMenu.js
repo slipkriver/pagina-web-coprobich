46f9215a679f86ce25bb267d568bdd3166d29f59
Elias Rivera <slipkriver@gmail.com>
2020-06-09 00:34:30 -0500

    Cambiando el nombre del boton para el acceso al sistema ERP y agregando la pagina empresa-calidad-certificado

--- assets/js/mainMenu.js
+++ assets/js/mainMenu.js
@@ -1,9 +1,3 @@
-/**
-* Template Name: Green - v2.0.0
-* Template URL: https://bootstrapmade.com/green-free-one-page-bootstrap-template/
-* Author: BootstrapMade.com
-* License: https://bootstrapmade.com/license/
-*/
 !(function($) {
   "use strict";
 
